5d863058a3219e038d22666671fff97c5fc73329
channacy <68759286+channacy@users.noreply.github.com>
2024-04-21 08:40:04 -0400

    load reports data

--- client/components/ReportsTable.tsx
+++ client/components/ReportsTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
-export default function ReportsTable() {
+import prisma from '@/lib/prisma';
+import { useEffect, useState } from 'react';
+export default function ReportsTable ({data}) {
   return (
     <div>
         <div className="relative overflow-x-auto">
@@ -11,7 +12,7 @@ export default function ReportsTable() {
                             Date
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Time
+                            Title
                         </th>
                         <th scope="col" className="px-6 py-3">
                             Technical Device
@@ -25,57 +26,25 @@ export default function ReportsTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
-                        </th>
-                        <td className="px-6 py-4">
-                            Silver
-                        </td>
-                        <td className="px-6 py-4">
-                            Laptop
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                    </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
-                        </th>
-                        <td className="px-6 py-4">
-                            White
-                        </td>
-                        <td className="px-6 py-4">
-                            Laptop PC
-                        </td>
-                        <td className="px-6 py-4">
-                            $1999
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                    </tr>
-                    <tr className="bg-white dark:bg-gray-800">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
-                        </th>
-                        <td className="px-6 py-4">
-                            Black
-                        </td>
-                        <td className="px-6 py-4">
-                            Accessories
-                        </td>
-                        <td className="px-6 py-4">
-                            $99
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                    </tr>
+                    {data.map((item, index) => (
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={index}>
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {item.date}
+                            </th>
+                            <td className="px-6 py-4">
+                                {item.title}
+                            </td>
+                            <td className="px-6 py-4">
+                                {item.device}
+                            </td>
+                            <td className="px-6 py-4">
+                                {item.description}
+                            </td>
+                            <td className="px-6 py-4">
+                                {item.solution}
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
--- client/pages/reports/index.tsx
+++ client/pages/reports/index.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import Header from '@/components/Header'
 import ReportsTable from '@/components/ReportsTable'
+import prisma from '@/lib/prisma'
+
+async function getReports() {
+  const reports = await prisma.report.findMany({
+    where: { published: true },
+  });
+  return reports;
+}
+
 export default function reports() {
+  const [reportsData, setReportsData] = useState([]);
+
+  useEffect(() => {
+    const fetchReports = async () => {
+      try {
+        const reports = await getReports();
+        setReportsData(reports);
+      } catch (error) {
+        console.error('Error fetching reports:', error);
+      }
+    };
+
+    fetchReports();
+  }, []);
+
   return (
     <>
       <Header/>
       <div className="bg-black flex flex-col h-screen px-10 py-5">
-        <ReportsTable/>
+        <ReportsTable data={reportsData}/>
       </div>
     </>
   )
 }
+
